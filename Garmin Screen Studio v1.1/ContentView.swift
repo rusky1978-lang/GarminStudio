80630138dabb7d0c56e1898e5297565fe7dd9c33
Russell Gosling <cyclingwithruss@gmail.com>
2026-07-07 04:56:42 +0100

    Initial Commit

---
+++ Garmin Screen Studio v1.1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Garmin Screen Studio v1.1
+//
+//  Created by Russell Gosling on 07/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Garmin Screen Studio v1.1/Garmin_Screen_Studio_v1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Garmin_Screen_Studio_v1_1App.swift
+//  Garmin Screen Studio v1.1
+//
+//  Created by Russell Gosling on 07/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Garmin_Screen_Studio_v1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
